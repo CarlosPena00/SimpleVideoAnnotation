f9479aa342d9317593776e1f8b58db5b5c6548c8
heitorrapela <hrm@cin.ufpe.br>
2018-03-18 23:26:54 -0300

    Add multi box

--- VideoAnnotation.py
+++ VideoAnnotation.py
@@ -10,7 +10,6 @@ if(cv2v[0] >= '3'):
 elif(cv2v[0] == '2'):
     flagCapturePosFrame = cv2.cv.CV_CAP_PROP_POS_FRAMES
 
-
 if len(sys.argv) == 2:
     videoName = sys.argv[1]
 else:
@@ -23,9 +22,9 @@ else:
         sys.exit("Error: Video path not provided and default doesnt exist")
 
 foldName = videoName.split('.')[0]
-foldLabel = foldName+'/labels'
-foldJpeg = foldName+'/JPEGImages'
-foldGT = foldName+'/Ground'
+foldLabel = foldName + '/labels'
+foldJpeg = foldName + '/JPEGImages'
+foldGT = foldName + '/Ground'
 
 if not os.path.exists(foldName):
     os.mkdir(foldName)
@@ -37,10 +36,11 @@ if not os.path.exists(foldGT):
     os.mkdir(foldGT)
           
 drawRect = False
-startRect = (-1, -1)
-endRect = (-1, -1)
+startRect = []
+endRect = []
 key = -1
 color = [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255),(0,0,0),(255,255,255)]
+actual = 0
 
 def draw(event,x,y,flags,param):
     global drawRect
@@ -50,23 +50,26 @@ def draw(event,x,y,flags,param):
     global oriFrame
     global key
     global color
+    global actual
     
     if event == cv2.EVENT_LBUTTONDOWN:
         drawRect = True
-        startRect = (x, y)
-        endRect = (x, y)
+        startRect.append((x, y))
+        endRect.append((x, y))
+        actual += 1
     elif event == cv2.EVENT_LBUTTONUP:
         drawRect = False
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawRect:
-            endRect = (x, y)
+            endRect[actual-1] = (x, y)
             frame = oriFrame.copy()
             iClass = cv2.getTrackbarPos('ID','VideoTag')
-            cv2.rectangle(frame, startRect, endRect, color[iClass])
+            for values in range(0,len(startRect)):
+                cv2.rectangle(frame, startRect[values], endRect[values], color[iClass])
     elif event == cv2.EVENT_RBUTTONDOWN:
         frame = oriFrame.copy()
-        startRect = (-1, -1)
-        endRect = (-1, -1)
+        startRect[actual-1] = (-1, -1)
+        endRect[actual-1] = (-1, -1)
 
 def nothing(x):
     pass
@@ -92,65 +95,70 @@ while(cap.isOpened() and ret ):
     iClass = cv2.getTrackbarPos('ID','VideoTag')
     jump = cv2.getTrackbarPos('Jump','VideoTag')
     
-    cv2.rectangle(frame, startRect, endRect, color[iClass], 2)
+    for values in range(0,len(startRect)):
+        cv2.rectangle(frame, startRect[values], endRect[values], color[iClass],2)
+
     cv2.imshow("VideoTag", frame)
 
     key = (cv2.waitKey(1) & 0xFF)
     if key == ord('q'):
         break
     if key == ord('w'):
-        startRect = (startRect[0], startRect[1]-jump)
-        endRect = (endRect[0], endRect[1]-jump)
+        startRect[actual-1] = (startRect[actual-1][0], startRect[actual-1][1]-jump)
+        endRect[actual-1] = (endRect[actual-1][0], endRect[actual-1][1]-jump)
     if key == ord('s'):
-        startRect = (startRect[0], startRect[1]+jump)
-        endRect = (endRect[0], endRect[1]+jump)
+        startRect[actual-1] = (startRect[actual-1][0], startRect[actual-1][1]+jump)
+        endRect[actual-1] = (endRect[actual-1][0], endRect[actual-1][1]+jump)
     if key == ord('a'):
-        startRect = (startRect[0]-jump, startRect[1])
-        endRect = (endRect[0]-jump, endRect[1])
+        startRect[actual-1] = (startRect[actual-1][0]-jump, startRect[actual-1][1])
+        endRect[actual-1] = (endRect[actual-1][0]-jump, endRect[actual-1][1])
     if key == ord('d'):
-        startRect = (startRect[0]+jump, startRect[1])
-        endRect = (endRect[0]+jump, endRect[1])
+        startRect[actual-1] = (startRect[actual-1][0]+jump, startRect[actual-1][1])
+        endRect[actual-1] = (endRect[actual-1][0]+jump, endRect[actual-1][1])
     if key == ord('6'):
-        startRect = (startRect[0]-jump, startRect[1])
-        endRect = (endRect[0]+jump, endRect[1])
+        startRect[actual-1] = (startRect[actual-1][0]-jump, startRect[actual-1][1])
+        endRect[actual-1] = (endRect[actual-1][0]+jump, endRect[actual-1][1])
     if key == ord('4'):
-        startRect = (startRect[0]+jump, startRect[1])
-        endRect = (endRect[0]-jump, endRect[1])
+        startRect[actual-1] = (startRect[actual-1][0]+jump, startRect[actual-1][1])
+        endRect[actual-1] = (endRect[actual-1][0]-jump, endRect[actual-1][1])
     if key == ord('8'):
-        startRect = (startRect[0], startRect[1]-jump)
-        endRect = (endRect[0], endRect[1]+jump)
+        startRect[actual-1] = (startRect[actual-1][0], startRect[actual-1][1]-jump)
+        endRect[actual-1] = (endRect[actual-1][0], endRect[actual-1][1]+jump)
     if key == ord('2'):
-        startRect = (startRect[0], startRect[1]+jump)
-        endRect = (endRect[0], endRect[1]-jump)
+        startRect[actual-1] = (startRect[actual-1][0], startRect[actual-1][1]+jump)
+        endRect[actual-1] = (endRect[actual-1][0], endRect[actual-1][1]-jump)
     if key == (32):
-        frameWidth, frameHeight = abs(startRect[0]-endRect[0]), abs(startRect[1]-endRect[1])
-        xCenter, yCenter = abs((startRect[0]+endRect[0])/2.0), abs((startRect[1]+endRect[1])/2.0)
-        xVoc, yVoc = xCenter/width, yCenter/height
+        vocLabel = []
+        for values in range(0,len(startRect)):
+            frameWidth, frameHeight = abs(startRect[values-1][0]-endRect[values-1][0]), abs(startRect[values-1][1]-endRect[values-1][1])
+            xCenter, yCenter = abs((startRect[values-1][0]+endRect[values-1][0])/2.0), abs((startRect[values-1][1]+endRect[values-1][1])/2.0)
+            xVoc, yVoc = xCenter/width, yCenter/height
+            
+            actual = cap.get(flagCapturePosFrame)
+            skip = cv2.getTrackbarPos('SkipFrames','VideoTag')
+            cap.set(flagCapturePosFrame,actual+skip)
+            
+            if frameWidth < 2 or frameHeight < 2:
+                ret, oriFrame = cap.read()  
+                continue
         
-        actual = cap.get(flagCapturePosFrame)
-        skip = cv2.getTrackbarPos('SkipFrames','VideoTag')
-        cap.set(flagCapturePosFrame,actual+skip)
-        
-        if frameWidth < 2 or frameHeight <2:
-            ret, oriFrame = cap.read()  
-            continue
-    
-        widthVoc = float(frameWidth)/width
-        heightVoc = float(frameHeight)/height
-        iClass = cv2.getTrackbarPos('ID','VideoTag')
-        vocClass = (iClass, xVoc,yVoc,  widthVoc, heightVoc, '\n')
-        vocLabel = ' '.join(str(e) + ' ' for e in vocClass)
-        fileName = "/{:06d}.jpg".format(framePos)
-        cv2.imwrite(foldGT+fileName, frame)
-        fileName = "/{:06d}.jpg".format(framePos)
-        cv2.imwrite(foldJpeg+fileName, oriFrame)
-                
-        ret, oriFrame = cap.read()  
-        
-        with open(foldLabel+"/{:06d}.txt".format(framePos), 'w') as f:
-            f.write(vocLabel)
-        framePos += 1
+            widthVoc = float(frameWidth)/width
+            heightVoc = float(frameHeight)/height
+            iClass = cv2.getTrackbarPos('ID','VideoTag')
+            vocClass = (iClass, xVoc,yVoc,  widthVoc, heightVoc, '\n')
+            vocLabel.append(' '.join(str(e) + '' for e in vocClass))
+            fileName = "/{:06d}.jpg".format(framePos)
+            cv2.imwrite(foldGT+fileName, frame)
+            fileName = "/{:06d}.jpg".format(framePos)
+            cv2.imwrite(foldJpeg+fileName, oriFrame)
 
+        with open(foldLabel+"/{:06d}.txt".format(framePos), 'w') as f:
+            for labels in vocLabel:
+                f.write(labels)
+
+        ret, oriFrame = cap.read()
+        framePos += 1
+    
 
 command = 'ls -d '+ os.getcwd() +'/{}/JPEGImages/* > '.format(foldName) + os.getcwd() + '/{}/imgList.txt'.format(foldName)
 os.system(command)
